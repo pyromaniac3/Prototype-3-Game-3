6ecb172956effa1f73ef529e6eba993d7d2f72b5
itspebbleboy <itspebbleboy@gmail.com>
2023-12-12 17:50:10 -0800

    player movement, burgers reset after rounds, can't eat more than one borger per round

--- src/scenes/Room.js
+++ src/scenes/Room.js
@@ -1,6 +1,10 @@
 class Room extends Phaser.Scene{
     constructor(){
         super("roomScene");
+        this.targetPosition = null;
+        this.isPlayerMoving = false;
+        this.currBorger = null;
+        this.foods = [];
     }
     preload(){
         this.load.image("food", "./assets/food.png");
@@ -20,8 +24,7 @@ class Room extends Phaser.Scene{
         let room = this.add.sprite(0,600,"background").setOrigin(0,1).setScale(0.5);
 
         // add in player sprite depth is to make sure its in front of the food
-        let player = this.add.sprite(50,600,"playerNormal").setOrigin(0,1).setDepth(10);
-      
+        this.player = this.add.sprite(50,600,"playerNormal").setOrigin(.5,1).setDepth(10);
         // round counter
         this.roundsLeft = 3;
 
@@ -41,8 +44,7 @@ class Room extends Phaser.Scene{
                     // set the size state to big and mak player big
                     this.currSizeState = this.sizeState.BIG;
                     console.log("am big");
-                    player.setTexture("playerBig");
-                    //player.setScale(3);// double the size
+                    this.player.setTexture("playerBig");
                 }
             },
             NORMAL:{
@@ -51,8 +53,7 @@ class Room extends Phaser.Scene{
                     // set the state to normal and revert size to regular
                     this.currSizeState = this.sizeState.NORMAL;
                     console.log("am normal");
-                    player.setTexture("playerNormal");
-                    //player.setScale(1); // normal scale
+                    this.player.setTexture("playerNormal");
                 }
             },
             SMALL:{
@@ -60,9 +61,8 @@ class Room extends Phaser.Scene{
                 enter: () =>{
                     // set state to small and scale to half size
                     this.currSizeState = this.sizeState.SMALL;
-                    console.log("am small")
-                    player.setTexture("playerSmall");
-                    //player.setScale(0.5); // half size
+                    console.log("am small");
+                    this.player.setTexture("playerSmall");
                 }
             } 
         }   
@@ -73,9 +73,17 @@ class Room extends Phaser.Scene{
         const food2 = new Food(this, foodX +260, foodY, 'food', [this.sizeState.BIG,this.sizeState.SMALL,this.sizeState.NORMAL], this.player).setScale(0.5);
         const food3 = new Food(this, foodX +520, foodY, 'food', [this.sizeState.BIG,this.sizeState.SMALL,this.sizeState.NORMAL], this.player).setScale(0.5);
         //#endregion
-        
+        this.foods[0] = food1;
+        this.foods[1] = food2;
+        this.foods[2] = food3;
         this.sizeState.NORMAL.enter();// enter normal to start with
 
+        // Global click listener
+        this.input.on('pointerdown', (pointer) => {
+            if (!this.isClickOnFood(pointer)) {
+                this.stopPlayerMovement();
+            }
+        });
     }
 
     onTimerComplete() {
@@ -83,36 +91,57 @@ class Room extends Phaser.Scene{
     }
 
     update(){
-
         // add a timer for 10 seconds to choose a door and a food
         // if when food is eaten and timer is over door gets opened and if the player isnt caught refill
         // the food and keep going until caught
-
         var remaining = this.timer.getRemainingSeconds();
         const remainingTime = Math.floor(remaining); // round down
-
         this.firstText.setText("Quick! They're coming!");
         this.secondText.setText(remainingTime + " seconds remaining");
-
         if(remainingTime == 0) {
             this.firstText.setText("");
             this.secondText.setText("");
             // implement scene here where the person comes to the door
-
+            
             this.roundsLeft--;
             this.timer = this.time.delayedCall(11000, this.onTimerComplete, [], this);
             this.firstText.setText("Quick! They're coming!");
             this.secondText.setText(remainingTime + " seconds remaining");
-
+            this.foods.forEach( (item) => {
+                item.setRandomState();
+                item.bite(1);
+            })
         }
-
         if(this.roundsLeft <= 0) {
             this.firstText.setText("");
             this.secondText.setText("");
             // player wins
             this.winText.setText("You win!");
         }
+        if (this.isPlayerMoving) {
+            const speed = 3; // speed of player
+            this.player.x +=( speed * Math.sign(this.targetPosition.x - this.player.x));
 
-
+            if(this.targetPosition != null && this.currBorger.active && (Math.abs(Math.abs(this.targetPosition.x) - Math.abs(this.player.x)) < 3)){
+                this.isPlayerMoving = false;
+                this.targetPosition = null;
+                this.currBorger.sizeState.enter();
+                this.currBorger.bite(0);
+                this.foods.forEach( (item) => {
+                    item.off();
+                })
+            }
+        }
     }
+
+    isClickOnFood(pointer) {
+        return this.foods.some(food => food.getBounds().contains(pointer.x, pointer.y));
+    }
+
+    stopPlayerMovement() {
+        this.isPlayerMoving = false;
+        this.targetPosition = null;
+        this.currBorger = null;
+    }
+
 }
